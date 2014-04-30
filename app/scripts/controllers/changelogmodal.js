5bee95784ad40cf3091c8e47d94fd9676abfdb19
Sean Bloomfield <sbloomfield@theonion.com>
2014-04-30 13:40:30 -0500

    getting changelog

--- app/scripts/controllers/changelogmodal.js
+++ app/scripts/controllers/changelogmodal.js
@@ -1,7 +1,12 @@
 'use strict';
 
 angular.module('bulbsCmsApp')
-  .controller('ChangelogmodalCtrl', function ($scope, $http, $modalInstance, article) {
+  .controller('ChangelogmodalCtrl', function ($scope, $http, $modalInstance, ContentApi, article) {
     $scope.article = article;
+
+    ContentApi.all('log').getList({content: article.id}).get(function (data) {
+      console.log(data)
+      $scope.changelog = data;
+    });
 
   });
