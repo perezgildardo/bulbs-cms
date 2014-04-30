bf1cabb063ca5fe96f747b3bb2245c241316fad4
Sean Bloomfield <sbloomfield@theonion.com>
2014-04-30 16:20:00 -0500

    oops: getList returns a promise

--- app/scripts/controllers/changelogmodal.js
+++ app/scripts/controllers/changelogmodal.js
@@ -4,8 +4,7 @@ angular.module('bulbsCmsApp')
   .controller('ChangelogmodalCtrl', function ($scope, $http, $modalInstance, ContentApi, article) {
     $scope.article = article;
 
-    ContentApi.all('log').getList({content: article.id}).get(function (data) {
-      console.log(data)
+    ContentApi.all('log').getList({content: article.id}).then(function (data) {
       $scope.changelog = data;
     });
 
