1ad78b8b436c890c2d6271abdc660a6b1b9d9f11
Sean Bloomfield <sbloomfield@theonion.com>
2014-06-18 15:55:34 -0500

    betty upload

--- app/scripts/controllers/videothumbnailmodal.js
+++ app/scripts/controllers/videothumbnailmodal.js
@@ -1,19 +1,29 @@
 'use strict';
 
 angular.module('bulbsCmsApp')
-  .controller('VideothumbnailmodalCtrl', function ($scope, $http, $modalInstance, Zencoder, videoId, posterUrl, VIDEO_THUMBNAIL_URL) {
+  .controller('VideothumbnailmodalCtrl', function ($scope, $http, $modalInstance, Zencoder, videoId, posterUrl, VIDEO_THUMBNAIL_URL, STATIC_IMAGE_URL) {
     var DEFAULT_THUMBNAIL = 4;
     var MAX_THUMBNAIL = 19;
     $scope.posterUrl = posterUrl || compilePosterUrl(DEFAULT_THUMBNAIL);
+    $scope.uploadedImage = {id: null};
+    $scope.mode = 'still';
 
     $scope.$watch('posterUrl', function(){
       var defaultUrl = VIDEO_THUMBNAIL_URL.replace('{{video}}', videoId);
       var thumbnailIndex = defaultUrl.indexOf('{{thumbnail}}');
       if($scope.posterUrl.indexOf(defaultUrl.substr(0, thumbnailIndex)) === 0){
         $scope.currentThumbnail = Number($scope.posterUrl.substr(thumbnailIndex, 4));
-        console.log($scope.currentThumbnail)
+        $scope.uploadedImage.id = null;
       }else{
         $scope.currentThumbnail = false;
+      }
+    });
+
+    $scope.$watch('uploadedImage.id', function(){
+      console.log("new uploadedImage.id")
+      console.log($scope.uploadedImage)
+      if($scope.uploadedImage.id){
+        $scope.posterUrl = STATIC_IMAGE_URL.replace('{{image}}', $scope.uploadedImage.id);
       }
     });
 
