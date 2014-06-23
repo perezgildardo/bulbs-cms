4acc59584eb4ec4d834653ce8f440990a440603a
Sean Bloomfield <sbloomfield@theonion.com>
2014-06-23 17:12:55 -0500

    tests

--- test/spec/directives/encode-status.js
+++ test/spec/directives/encode-status.js
@@ -12,9 +12,4 @@ describe('Directive: encodeStatus', function () {
     scope = $rootScope.$new();
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<encode-status></encode-status>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the encodeStatus directive');
-  }));
 });
