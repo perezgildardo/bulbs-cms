5e902f6ea12ad76bc587e01acc11784685f733da
Chris Sinchok <csinchok@theonion.com>
2014-07-30 12:44:29 -0500

    Down to 336

--- app/scripts/services/permissionsinterceptor.js
+++ app/scripts/services/permissionsinterceptor.js
@@ -5,23 +5,23 @@
 angular.module('bulbsCmsApp').factory('PermissionsInterceptor', function ($q, $injector, routes) {
   return {
     responseError: function (rejection) {
-      if(rejection.config.noPermissionIntercept) {
+      if (rejection.config.noPermissionIntercept) {
         return $q.when(rejection);
-      }else{
-        $injector.invoke(function($modal){
+      } else {
+        $injector.invoke(function ($modal) {
           if (rejection.status === 403) {
             if (rejection.data && rejection.data.detail && rejection.data.detail.indexOf('credentials') > 0) {
               $modal.open({
                 templateUrl: routes.PARTIALS_URL + 'modals/login-modal.html',
                 controller: 'LoginmodalCtrl'
               });
-            }else{
+            } else {
               var detail = rejection.data && rejection.data.detail || 'Forbidden';
               $modal.open({
                 templateUrl: routes.PARTIALS_URL + 'modals/403-modal.html',
                 controller: 'ForbiddenmodalCtrl',
                 resolve: {
-                  detail: function(){ return detail; }
+                  detail: function () { return detail; }
                 }
               });
             }
