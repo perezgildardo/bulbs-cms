24180643866142db97cfeb80b5d15f05113018fa
Chris Sinchok <csinchok@theonion.com>
2014-10-06 16:33:02 -0500

    Look, the selects work!

--- app/scripts/controllers/contributions.js
+++ app/scripts/controllers/contributions.js
@@ -12,7 +12,7 @@ angular.module('bulbsCmsApp')
     $scope.save = save;
     $scope.add = add;
     $scope.remove = remove;
-    $scope.roles = [];
+    $scope.roles = {};
 
     function save() {
       console.log($scope.contributions);
@@ -32,13 +32,27 @@ angular.module('bulbsCmsApp')
 
     function getRoles() {
       return ContributionRoleService.getList().then(function(roles){
-        $scope.roles = roles;
+        $scope.roles = {};
+        roles.forEach(function(role) {
+          $scope.roles[role.id] = role.name;
+        });
+        getContributions();
       });
     }
 
     function getContributions() {
       return ContentService.one($scope.contentId).all('contributions').getList().then(function(contributions) {
+        for (var i in contributions) {
+          if (contributions[i] === null || contributions[i].role === undefined) {
+            continue;
+          }
+          var roleId = contributions[i].role.id;
+          if (roleId && $scope.roles.hasOwnProperty(roleId)) {
+            contributions[i].role = $scope.roles[roleId];
+          }
+        }
         $scope.contributions = contributions;
+
       });
     }
 
@@ -46,9 +60,6 @@ angular.module('bulbsCmsApp')
       $scope.contributions.splice(index, 1);
     }
 
-
-    getContributions();
     getRoles();
-
 
   });
