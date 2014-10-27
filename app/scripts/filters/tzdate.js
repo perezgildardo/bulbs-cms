6cd692e80b0c69c085cacba003e9d91c0c1fb0e2
Adam Wentz <awentz@theonion.com>
2014-10-27 10:36:37 -0500

    Timezone: removed most usage of TIMEZONE_LABEL

--- app/scripts/filters/tzdate.js
+++ app/scripts/filters/tzdate.js
@@ -1,16 +1,13 @@
 'use strict';
 
 angular.module('bulbsCmsApp')
-  .filter('tzDate', function (dateFilter, moment, TIMEZONE_LABEL) {
+  .filter('tzDate', function (dateFilter, moment) {
     return function (input, format) {
       if (!input) {
         return '';
       }
-      var newdate = moment(input).format('YYYY-MM-DDTHH:mm');
+      var newdate = moment(input).format('YYYY-MM-DDTHH:mm z');
       var formattedDate = dateFilter(newdate, format);
-      if (format.toLowerCase().indexOf('h') > -1) {
-        formattedDate += ' ' + TIMEZONE_LABEL;
-      }
       return formattedDate;
     };
   });
