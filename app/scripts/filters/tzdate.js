fe42c0aa8e6c5d4be6be4db7a3aa2e8d5e046112
Adam Wentz <awentz@theonion.com>
2014-10-27 10:23:13 -0500

    Timezone: removed a bunch of TIMEZONE_OFFSET stuff

--- app/scripts/filters/tzdate.js
+++ app/scripts/filters/tzdate.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('bulbsCmsApp')
-  .filter('tzDate', function (dateFilter, moment, TIMEZONE_OFFSET, TIMEZONE_LABEL) {
+  .filter('tzDate', function (dateFilter, moment, TIMEZONE_LABEL) {
     return function (input, format) {
       if (!input) {
         return '';
       }
-      var newdate = moment(input).zone(TIMEZONE_OFFSET).format('YYYY-MM-DDTHH:mm');
+      var newdate = moment(input).format('YYYY-MM-DDTHH:mm');
       var formattedDate = dateFilter(newdate, format);
       if (format.toLowerCase().indexOf('h') > -1) {
         formattedDate += ' ' + TIMEZONE_LABEL;
